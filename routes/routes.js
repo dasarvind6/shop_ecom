aa3b98018249acf86bdde33c114717e1f47ee5e5
webplusera <dasarvind6@gmail.com>
2025-10-30 03:12:05 +0530

    Your descriptive commit message here

--- routes/routes.js
+++ routes/routes.js
@@ -145,7 +145,7 @@ router.post('/admin/import/allproducts/', importAllProAdmin);
 
 // Middleware function to check if the request is coming from an authorized domain
 function checkOrigin(req, res, next) {
-  const allowedOrigins = ['https://shopnew.servicekarwalo.com', 'https://shop.servicekarwalo.com/']; // Add your authorized domains here
+  const allowedOrigins = ['https://shopnew.servicekarwalo.com', 'https://shop.servicekarwalo.com', 'https://shopnew.servicekarwalo.com/', 'https://shop.servicekarwalo.com/']; // Add your authorized domains here
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
