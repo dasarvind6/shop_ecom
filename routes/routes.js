96aabed41e1e9b3a1e24136eaebeeb38cca628d8
dasarvind6 <dasarvind6@gmail.com>
2025-10-30 12:23:55 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -159,7 +159,7 @@ router.post('/admin/import/allproducts/', importAllProAdmin);
 // Middleware function to check if the request is coming from an authorized domain
 function checkOrigin(req, res, next) {
   const allowedOrigins = [
-  'https://shopnew.servicekarwalo.com', 'https://shop.servicekarwalo.com'
+  'https://shopnew.servicekarwalo.com', 'https://shop.servicekarwalo.com', 'https://shopadmin.servicekarwalo.com'
   ]; // Add your authorized domains here
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
